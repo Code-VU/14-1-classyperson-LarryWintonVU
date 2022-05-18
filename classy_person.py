4e0c084f1043839be82b376dcfbdb0ef4f6fa9cd
Larry Winton <Larry.Winton@vu.com>
2022-05-18 14:01:40 -0500

    14.1 type hinting and type coercion

--- classy_person.py
+++ classy_person.py
@@ -7,21 +7,19 @@ don't forget the use of 'self' and to have the methods:
 4. count_to_age
 '''
 
-class Person :
+class Person:
     name = ""
     age = 0
 
-    def __init__(self, input_name, input_age) :
+    def __init__(self, input_age: int, input_name: str) :
         self.name = input_name
-        self.age = input_age
-        print(self.name, self.age)
+        self.age = int(input_age)
 
     def increase_age(self) :
         self.age = self.age + 1
-        print(self.age)
 
     def say_greeting(self) :
-        print('Hello world! My name is {0}!', self.name )
+        print('Hello world! My name is {0}!'.format(self.name ))
 
     def count_to_age(self) :
         for num in range (1,self.age+1) :
