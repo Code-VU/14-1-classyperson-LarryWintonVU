63464d6418fe22ee1a9c8c2f2b45a3469a616c7b
Larry Winton <Larry.Winton@vu.com>
2022-05-18 13:40:06 -0500

    14.1 first try

--- classy_person.py
+++ classy_person.py
@@ -7,9 +7,26 @@ don't forget the use of 'self' and to have the methods:
 4. count_to_age
 '''
 
-class Person:
+class Person :
+    name = ""
+    age = 0
 
+    def __init__(self, input_name, input_age) :
+        self.name = input_name
+        self.age = input_age
+        print(self.name, self.age)
 
+    def increase_age(self) :
+        self.age = self.age + 1
+        print(self.age)
+
+    def say_greeting(self) :
+        print('Hello world! My name is {0}!', self.name )
+
+    def count_to_age(self) :
+        print('do a loop to count from 1 to self.age')
+        for num in range (1,self.age+1) :
+            print(num)
 
 
 # You won't need to call anything here.
