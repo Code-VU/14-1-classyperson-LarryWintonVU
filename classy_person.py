2fee82f5a5930e6a29502366be3d277ecbe2dafd
Larry Winton <Larry.Winton@vu.com>
2022-05-19 12:00:50 -0500

    14.1 try-except and unit testing

--- classy_person.py
+++ classy_person.py
@@ -7,12 +7,19 @@ don't forget the use of 'self' and to have the methods:
 4. count_to_age
 '''
 
+import unittest
+import io
+import sys
+
 class Person:
     name = ""
     age = 0
 
     def __init__(self, input_age: int, input_name: str) :
-        self.age = int(input_age)
+        try :
+            self.age = int(input_age)
+        except :
+            print('Age input was not an integer number.')
         self.name = input_name
 
     def increase_age(self) :
@@ -28,3 +35,36 @@ class Person:
 
 
 # You won't need to call anything here.
+class TestPersonMethods(unittest.TestCase) :
+    def test_age(self):
+        larry = Person(10, 'Larry Winton')
+        self.assertEqual(larry.age, 10)
+
+    def test_age_nan(self):
+        larry = Person('NaN', 'Larry Winton')
+        self.assertNotEqual(larry.age, 'NaN')
+
+    def test_increase_age(self) :
+        larry = Person(10, 'Larry Winton')
+        larry.increase_age()
+        self.assertEqual(larry.age, 11)
+
+    def test_say_greeting(self) :
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        larry = Person(10, 'Larry Winton')
+        larry.say_greeting()
+        sys.stdout = sys.__stdout__
+        self.assertTrue('Larry Winton' in capturedOutput.getvalue())
+
+    def test_count_to_age(self) :
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        larry = Person(10, 'Larry Winton')
+        larry.count_to_age()
+        sys.stdout = sys.__stdout__
+        self.assertTrue('10' in capturedOutput.getvalue())        
+
+# comment out for submission to class assignment
+# if __name__ == '__main__' :
+#     unittest.main()
